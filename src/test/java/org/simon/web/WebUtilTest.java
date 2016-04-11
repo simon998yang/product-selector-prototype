988dc59b2bf99bc1aaf8a0a1162270cbaf0ce8f7
Simon Yang <simon998.yang@gmail.com>
2016-04-11 16:31:38 +0930

    add assert to WebUtilTest

--- src/test/java/org/simon/web/WebUtilTest.java
+++ src/test/java/org/simon/web/WebUtilTest.java
@@ -5,6 +5,8 @@ import junit.framework.TestCase;
 public class WebUtilTest extends TestCase {
 	public void testLoadConfigFileAsString() {
 		String s = new WebUtil().loadConfigFileAsString();
+		String subS = s.substring(0, "<productAdvisor>".length());
+		assertEquals("<productAdvisor>", subS);
 	}
 
 }
